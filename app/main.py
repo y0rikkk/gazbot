614fb34fb89527826c6048c9543f20742727cbc0
Iaroslav Belous <yarick2205@gmail.com>
2025-11-22 13:46:53 +0300

    add cov and fix warnings

--- app/main.py
+++ app/main.py
@@ -68,7 +68,7 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
         f"Validation Error on {request.method} {request.url.path}: {exc.errors()}"
     )
     return JSONResponse(
-        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
         content={"detail": exc.errors()},
     )
 
